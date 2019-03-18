9738cc34fbd926271207a9d3d520b5fcdea9735b
yuri <yurivladdurain@gmail.com>
2019-03-18 17:30:57 +0300

    - updated default services repository url - updated cyberdiscussion model

--- src/main/java/io/golos/cyber4j/model/CyberDiscussion.kt
+++ src/main/java/io/golos/cyber4j/model/CyberDiscussion.kt
@@ -3,36 +3,46 @@ package io.golos.cyber4j.model
 import java.math.BigInteger
 import java.util.*
 
-data class CyberDiscussion(val contentId: DiscussionId,
-                           val author: DiscussionAuthor,
-                           val community: CyberCommunity,
-                           val content: DiscussionContent,
-                           val votes: DiscussionVotes,
-                           val comments: DiscussionCommentsCount,
-                           val payout: DiscussionPayout,
-                           val postId: ParentId?,
-                           val parentCommentId: ParentId?,
-                           val meta: DiscussionMetadata)
+data class CyberDiscussion(
+    val contentId: DiscussionId,
+    val author: DiscussionAuthor?,
+    val community: CyberCommunity,
+    val content: DiscussionContent,
+    val votes: DiscussionVotes,
+    val stats: DiscussionStats,
+    val payout: DiscussionPayout,
+    val postId: ParentId?,
+    val parentCommentId: ParentId?,
+    val meta: DiscussionMetadata
+)
 
 data class DiscussionAuthor(val userId: CyberName, val username: String)
 
-data class DiscussionId(val userId: String,
-                        val permlink: String,
-                        val refBlockNum: Long)
+data class DiscussionId(
+    val userId: String,
+    val permlink: String,
+    val refBlockNum: Long
+)
 
-data class DiscussionCommentsCount(val count: Long)
 
+data class DiscussionStats(val commentsCount: Long, val wilson: DiscussionWilson)
 
-data class DiscussionContent(val title: String, val body: ContentBody, val metadata: Any)
+data class DiscussionWilson(val hot: Double, val trending: Double)
 
-data class ContentBody(val preview: String?,
-                       val full: String?)
+data class DiscussionContent(val title: String, val body: ContentBody, val metadata: Any?)
+
+data class ContentBody(
+    val preview: String?,
+    val full: String?
+)
 
 data class DiscussionMetadata(val time: Date)
 
 data class DiscussionPayout(val rShares: BigInteger)
 
-data class DiscussionVotes(val hasUpVote: Boolean,
-                           val hasDownVote: Boolean)
+data class DiscussionVotes(
+    val hasUpVote: Boolean,
+    val hasDownVote: Boolean
+)
 
 data class ParentId(val userId: CyberName, val permlink: String, val refBlockNum: Int)
